57f10ada1b7d584488fd370de55ed1b2a19b4bd6
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-06-01 11:46:21 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "TEXT": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    }
+}
